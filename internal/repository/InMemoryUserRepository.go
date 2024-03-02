aaeab2c287e86f8a41ea8ca9bd176aa30362711a
YutoOkawa <yuutoowata.0305@gmail.com>
2024-03-02 16:37:21 +0900

    Add GetUser

--- internal/repository/InMemoryUserRepository.go
+++ internal/repository/InMemoryUserRepository.go
@@ -8,6 +8,10 @@ type InMemoryUserRepository struct {
 	Users []webauthn.User
 }
 
+func (i *InMemoryUserRepository) GetUser() (webauthn.User, error) {
+	return i.Users[0], nil
+}
+
 func (i *InMemoryUserRepository) SaveUser(user webauthn.User) error {
 	i.Users = append(i.Users, user)
 	return nil
--- internal/repository/UserRepository.go
+++ internal/repository/UserRepository.go
@@ -3,5 +3,6 @@ package repository
 import "github.com/go-webauthn/webauthn/webauthn"
 
 type UserRepository interface {
+	GetUser() (webauthn.User, error)
 	SaveUser(user webauthn.User) error
 }
